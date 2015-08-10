5f2f2343e6f2a8835418f6d2278b2f0ef8ef24fe
danutzthe <daniel.dumbrava27@yahoo.com>
2015-08-10 16:44:08 +0300

    First commit

---
+++ views/site/entry-confirm.php
@@ -0,0 +1,37 @@
+<?php
+use yii\helpers\Html;
+?>
+<p>You have entered the following information:</p>
+
+<ul>
+    <li><label>Name</label>: <?= Html::encode($model->name) ?></li>
+    <li><label>Email</label>: <?= Html::encode($model->email) ?></li>
+</ul>
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">You have entered the following information</h4>
+      </div>
+      <div class="modal-body">
+        <ul>
+			<li><label>Name</label>: <?= Html::encode($model->name) ?></li>
+			<li><label>Email</label>: <?= Html::encode($model->email) ?></li>
+		</ul>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
